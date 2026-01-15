9344061b55abc74b9642f1c36aa6bfb746f13159
Prakalya R G <prakalyaprakalya72@gmail.com>
2026-01-15 18:15:55 +0530

    Add files via upload

---
+++ Elevate/Task_1/src/elevate_task_1/task_1.java
@@ -0,0 +1,21 @@
+package elevate_task_1;
+
+import java.util.Scanner;
+
+public class task_1 {
+	    // main method - entry point of program
+	    public static void main(String[] args) {
+            Scanner sc=new Scanner (System.in);
+            System.out.println("Enter the name :");
+            String str=sc.next();
+	        System.out.println("Hello Java World!");
+
+	        // printing command line arguments
+	        System.out.println(str+"Task-1");
+
+	        }
+	    }
+	
+
+
+
